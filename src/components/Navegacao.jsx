b25f690590c22cbec3e9f655dffd36c0b3606556
Natan <natan.nh20@gmail.com>
2025-05-17 09:12:18 -0300

    gitpage url

--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -1,44 +1,46 @@
+import { Link } from "react-router-dom";
+
 function Navegacao() {
   return (
-    <nav class="sticky top-0 bg-yellow-400 shadow-md w-full">
-      <div class="max-w-screen-xl mx-auto flex items-center justify-between p-4">
-        <a href="/" class="flex items-center space-x-2">
-          <span class="text-xl font-semibold text-gray-800 hover:text-black transition-colors duration-300">
+    <nav className="sticky top-0 bg-yellow-400 shadow-md w-full">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between p-4">
+        <Link to="/" className="flex items-center space-x-2">
+          <span className="text-xl font-semibold text-gray-800 hover:text-black transition-colors duration-300">
             RenovaEnergy
           </span>
-        </a>
-        <ul class="flex space-x-8 text-gray-700 font-medium">
+        </Link>
+        <ul className="flex space-x-8 text-gray-700 font-medium">
           <li>
-            <a
-              href="/projeto"
-              class="hover:text-black transition-colors duration-300"
+            <Link
+              to="/projeto"
+              className="hover:text-black transition-colors duration-300"
             >
               Painéis
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/funcionalidade"
-              class="hover:text-black transition-colors duration-300"
+            <Link
+              to="/funcionalidade"
+              className="hover:text-black transition-colors duration-300"
             >
               Funcionalidade
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/saibaMais"
-              class="hover:text-black transition-colors duration-300"
+            <Link
+              to="/saibaMais"
+              className="hover:text-black transition-colors duration-300"
             >
               Saiba Mais
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/sobre"
-              class="hover:text-black transition-colors duration-300"
+            <Link
+              to="/sobre"
+              className="hover:text-black transition-colors duration-300"
             >
               Sobre
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
